78ccbf03ad5a897b21c470af2db8af2d3bb03fe2
Ian Reymond <ireymond0@csu.fullerton.edu>
2015-12-28 20:28:15 -0800

    working

--- hours.cpp
+++ hours.cpp
@@ -1,28 +1,82 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
 using namespace std;
 
-string openfile()
+// string openfile()
+// {
+//   string file;
+//   ifstream hrFile;
+//   hrFile.open("hours.txt");
+//   if(!hrFile.is_open())
+//   {
+//     cout << "Error: Could not open hours.txt" << endl;
+//   }
+//   while(!hrFile.eof())
+//   {
+//
+//   }
+//   hrFile.close();
+//   return file;
+// }
+
+// void writeFile()
+// {
+//   ofstream hrFile;
+//   hrFile.open("hours.txt", ios::app);
+//   if(!hrFile.is_open())
+//   {
+//     cout << "error";
+//   }
+//   for(int i = 0; i < 3; ++i)
+//   {
+//     hrFile << "Testing \n";
+//   }
+//   hrFile.close();
+// }
+
+void clockIn()
 {
-  string file;
-  ifstream hrFile;
-  hrFile.open("hours.txt");
+  ofstream hrFile;
+  string fileName;
+  char lunch;
+  cout << "Are you coming back from lunch? (y/n)";
+  cin >> lunch;
+
+  time_t t = time(0);
+  struct tm *now = localtime(&t);
+
+  fileName = "Pay starting " + (now->tm_mon+1) + '-' + (now->tm_mday);
+  hrFile.open(fileName, ios::app);
   if(!hrFile.is_open())
   {
-    cout << "Error: Could not open hours.txt" << endl;
+    cout << "ERROR: could not open the hours file";
   }
-  while(!hrFile.eof())
+
+  if(lunch == 'n')
   {
-    
+    hrFile << "START: " << (now->tm_mon+1) << "-" << (now->tm_mday) << endl;
+    hrFile << "in: " << (now->tm_hour) << ":" << (now->tm_min) << endl;
+  }
+  else
+  {
+    hrFile << "Clocked in at " << (now->tm_hour) << ":" << (now->tm_min) << endl;
   }
   hrFile.close();
-  return file;
 }
 
 int main(int argc, char** argv)
 {
-  cout << "** Luxion Clock Utility **" << endl;
+  char input;
+  cout << endl << "** Luxion Clock Utility **\n" << endl;
+
+  cout << "What would you like to do?" << endl;
+  cout << "i = clock-in\n" << "o = clock-out\n" << "d = daily hours\n" <<
+    "p = paycheck hours\n";
+  cin >> input;
+  if(input == 'i')
+    clockIn();
 
 
   return 0;
